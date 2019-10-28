e6878e071b9b4ca7171b674f94cc30363ad0e374
Jean Tubay <jean.tubay.92@gmail.com>
2019-10-27 21:14:42 -0400

    added local storage and made ls display

--- script.js
+++ script.js
@@ -6,24 +6,13 @@ $(function(){
     let year = d.getFullYear();
     let month = d.getMonth()+1;
     let day = d.getDate();
-    // let currentHours = d.getHours();
-    // let currentMinutes = d.getMinutes();
-    // let currentSeconds = d.getSeconds();
-
-    // currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
-    // currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
     
-    // let timeOfDay = (currentHours < 12) ? "AM" : "PM";
-
-    // currentHours = (currentHours > 12) ? currentHours - 12: currentHours;
-
-    // let hourOutput = currentHours + ":" + currentMinutes + ":" + currentSeconds 
     
 
 
-    let totalDays = new Date(year, month, 0).getDate();
+    // let totalDays = new Date(year, month, 0).getDate();
 
-    let output = (month<10 ? '0' : '') + month + '/' + (day<10 ? '0' : '') + day + '/' + year;
+    // let output = (month<10 ? '0' : '') + month + '/' + (day<10 ? '0' : '') + day + '/' + year;
 
     // let output =  `${month}/${day}/${year}`
     const m = moment();
@@ -33,19 +22,109 @@ $(function(){
 
 
 
-const btn = document.getElementById("btnOne")
-const ins = document.getElementById("insert")
 
-btn.onclick =function () {
-    
-    const key = ins.value;
+
+
+$("#btnOne").on("click", function(event) {
+    event.preventDefault()
+    const key = $("#insert").val()
+    // ins.value;
 
     if(key) {
-        localStorage.setItem("key", key);
+        localStorage.setItem("key", JSON.stringify(key));
+        location.reload();
+    }
+    
+    
+})
+
+const last = JSON.parse(localStorage.getItem("key"))
+    $("#displayOne").text(last)
+
+$("#btnTwo").on("click", function() {
+    const keyTwo = $("#insertTwo").val()
+    
+    if(keyTwo) {
+        localStorage.setItem("keyTwo", keyTwo);
         location.reload();
         
     }
-}
+})
+
+$("#btnThree").on("click", function() {
+    const keyThree = $("#insertThree").val()
+    
+    if(keyThree) {
+        localStorage.setItem("keyThree", keyThree);
+        location.reload();
+        
+    }
+})
+
+$("#btnFour").on("click", function() {
+    const keyFour = $("#insertFour").val()
+    
+    if(keyFour) {
+        localStorage.setItem("keyFour", keyFour);
+        location.reload();
+        
+    }
+})
+
+$("#btnFive").on("click", function() {
+    const keyFive = $("#insertFive").val()
+    
+    if(keyFive) {
+        localStorage.setItem("keyFive", keyFive);
+        location.reload();
+        
+    }
+})
+
+$("#btnSix").on("click", function() {
+    const keySix = $("#insertSix").val()
+    
+    if(keySix) {
+        localStorage.setItem("keySix", keySix);
+        location.reload();
+        
+    }
+})
+
+$("#btnSeven").on("click", function() {
+    const keySeven = $("#insertSeven").val()
+    
+    if(keySeven) {
+        localStorage.setItem("keySeven", keySeven);
+        location.reload();
+        
+    }
+})
+
+$("#btnEight").on("click", function() {
+    const keyEight = $("#insertEight").val()
+    
+    if(keyEight) {
+        localStorage.setItem("keyEight", keyEight);
+        location.reload();
+        
+    }
+})
+
+$("#btnNine").on("click", function() {
+    const keyNine = $("#insertNine").val()
+    
+    if(keyNine) {
+        localStorage.setItem("keyNine", keyNine);
+        location.reload();
+        
+    }
+})
+
+
+    
+    
+
 
     
 
